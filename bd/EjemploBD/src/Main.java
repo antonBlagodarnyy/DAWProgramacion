f9d489a9caa42ab5dd883e66afd708157f1ac594
antonBlagodarnyy <antonbgi98@gmail.com>
2024-04-09 12:26:45 +0200

    Corregidos los apuntes de acceso a bd en java

--- bd/EjemploBD/src/Main.java
+++ bd/EjemploBD/src/Main.java
@@ -1,36 +1,44 @@
-
+import java.sql.*;
 public class Main {
-	﻿
+
 
 	public static void main(String[] args) {
-	// Configuración de la conexión a la base de datos Oracle XE
-	String url="jdbc: oracle: thin: @localhost:1521:XE"; // URL de conexión 
-	// Intenta establecer la conexión
-	try (Connection conn = DriverManager.getConnection (url, "SYSTEM", "asd"); 
-			//Indicamos usuario y contraseña
-			
-			Statement stmt = conn.createStatement ()) {
-		// Declaración de la sentencia SQL
-		String sql = "INSERT INTO empleados (id, nombre, salario) + "VALUES ('3', 'CSCI', 3720)";
-		
-				// Ejecutar la sentencia SQL
-				stmt.executeUpdate(sql);
-				// Declaración de la sentencia SQL
-				String sql = "SELECT ID, Nombre, Salario FROM Empleados";
-				// Intenta establecer la conexión y ejecutar la consulta
-				try (Connection conn = DriverManager.getConnection (url, usuario, contraseña); Statement stmt = conn.createStatement ();
-				ResultSet rs = stmt.executeQuery (sql)) {
+		// Configuración de la conexión a la base de datos Oracle XE
+		String url = "jdbc:oracle:thin:@localhost:1521:XE"; // URL de conexión
+		String usuario = "SYSTEM"; // Usuario de la base de datos
+		String contraseña = "Admin2023"; // Contraseña de la base de datos
 
-				/*	
-				// Recorre el ResultSet e imprime los datos while (rs.next()) {
-				while(rs.next()) {
-				int id =rs.getInt("ID");
-				String nombre= rs.getString("Nombre");
-				
-				double salario =rs.getDouble ("Salario");
-				
-				System.out.println("ID: " + id + ", Nombre:
-				"
-				}
-*/
+		// Intenta establecer la conexión y ejecutar la consulta
+		try (Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+				Statement stmt = conn.createStatement()) {
+			// Declaración de la sentencia SQL para inserción
+			String insertSql = "INSERT INTO empleados (dni, nombre, salario) VALUES (3, 'CSCI', 3720)";
+			// Ejecutar la sentencia SQL para inserción
+			stmt.executeUpdate(insertSql);
+
+		} catch (SQLException e) {
+			System.out.println("Insert fallido");
+		}
+
+		try (Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+				Statement stmt = conn.createStatement()) {
+			// Declaración de la sentencia SQL
+			String selectSql = "SELECT DNI, NOMBRE, SALARIO FROM Empleados";
+			ResultSet rs = stmt.executeQuery (selectSql);
+
+			//Recorre el ResultSet e imprime los datos
+			while(rs.next()) {
+				int id =rs.getInt("DNI");
+				String nombre= rs.getString("NOMBRE");
+
+				int salario =rs.getInt ("SALARIO");
+
+				System.out.println("ID: " + id + ", Nombre: " + nombre + ", Salario: " + salario);
+			}
+		} catch (SQLException e) {
+			System.out.println("Seleccion fallida");
+		}
 	}
+}
+
+
